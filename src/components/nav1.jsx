f0265bb4faacb0d30b3844c916192fa4625f6570
Shukurxan <wukurulloabduqayumov777@gmail.com>
2023-02-24 16:34:57 +1000

    updates

--- src/components/nav1.jsx
+++ src/components/nav1.jsx
@@ -7,11 +7,9 @@ export default class nav1 extends Component {
     return (
       <div class="d-inline-block">
         <h1 class="d-block">
-            <div class="d-inline-block">
-              <span id='anim-write' class="typewrite text-warning d-block" data-period="2000" data-type='[ "Hi, Im Shukurkhan.", "I am Frontend Developer.", "I am Creative.", "I Love Design.", "I Love to Develop." ]'>
-                  <span class="wrap"></span>
-              </span>
-            </div>
+          <span id='anim-write' class="typewrite text-warning d-block" data-period="2000" data-type='[ "Hi, Im Shukurkhan.", "I am Frontend Developer.", "I am Creative.", "I Love Design.", "I Love to Develop." ]'>
+              <span class="wrap"></span>
+          </span>
         </h1>
       </div>
     )
