85409b655beb9e2b619c582c0ce3651f672e6b83
Shukurxan <wukurulloabduqayumov777@gmail.com>
2023-03-01 15:03:50 +1000

    update

--- src/components/nav1.jsx
+++ src/components/nav1.jsx
@@ -5,13 +5,18 @@ import '../js/nav1.js';
 export default class nav1 extends Component {
   render() {
     return (
-      <div>
-        <h1 class="d-block">
-          <span id='anim-write' class="typewrite text-warning d-block" data-period="2000" data-type='[ "Hi, Im Shukurkhan.", "I am Frontend Developer.", "I am Creative.", "I Love Design.", "I Love to Develop." ]'>
-              <span class="wrap"></span>
-          </span>
-        </h1>
-      </div>
+      <h1>
+        <a href="" class="typewrite" data-period="2000" data-type='[ "Hi, Im VipulM.", "I am Creative.", "I Love Design.", "I Love to Develop." ]'>
+          <span class="wrap"></span>
+        </a>
+      </h1>
+      // <div>
+      //   <h1 class="d-block">
+      //     <span id='anim-write' class="typewrite text-warning d-block" data-period="2000" data-type='[ "Hi, Im Shukurkhan.", "I am Frontend Developer.", "I am Creative.", "I Love Design.", "I Love to Develop." ]'>
+      //         <span class="wrap"></span>
+      //     </span>
+      //   </h1>
+      // </div>
     )
   }
 }
