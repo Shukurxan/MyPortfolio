b45a19e7b7705ccb0bcc6f9dfe467d8c86db8f7a
Shukurxan <wukurulloabduqayumov777@gmail.com>
2023-03-01 15:30:57 +1000

    update1

--- src/components/nav1.jsx
+++ src/components/nav1.jsx
@@ -5,11 +5,14 @@ import '../js/nav1.js';
 export default class nav1 extends Component {
   render() {
     return (
-      <h1>
-        <a href="" class="typewrite" data-period="2000" data-type='[ "Hi, Im VipulM.", "I am Creative.", "I Love Design.", "I Love to Develop." ]'>
-          <span class="wrap"></span>
-        </a>
-      </h1>
+      <div>
+        <h1>This pen is
+          <span
+            class="txt-rotate"
+            data-period="2000"
+            data-rotate='[ "nerdy.", "simple.", "pure JS.", "pretty.", "fun!" ]'></span>
+        </h1>
+      </div>
       // <div>
       //   <h1 class="d-block">
       //     <span id='anim-write' class="typewrite text-warning d-block" data-period="2000" data-type='[ "Hi, Im Shukurkhan.", "I am Frontend Developer.", "I am Creative.", "I Love Design.", "I Love to Develop." ]'>
