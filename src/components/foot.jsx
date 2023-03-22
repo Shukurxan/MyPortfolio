44dd8151eda731cc12af39fbaba87f0ab0621418
Shukurxan <wukurulloabduqayumov777@gmail.com>
2023-03-22 16:44:34 +1000

    linkedin

--- src/components/foot.jsx
+++ src/components/foot.jsx
@@ -17,6 +17,7 @@ export default class foot extends Component {
                     <a href="https://www.telegram.me/kali_pro"><i class="fa-brands fa-telegram fs-2"></i></a>
                     <a href="https://www.instagram.com/shukurxan777"><i class="fa-brands fa-instagram fs-2 text-danger"></i></a>
                     <a href="https://www.twitter.com/shukurxan777"><i class="fa-brands fa-twitter fs-2"></i></a>
+                    <a href="https://uz.linkedin.com/in/shukurulloh-abduqayumov-6a77a8250?trk=people_directory"><i class="fa-brands fa-linkedin-in fs-2"></i></a>
                 </div>
             </div>
         </div>
