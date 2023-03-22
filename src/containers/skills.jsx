094df5be4923c3ecfadc08b2e1e1a5f8126675f1
Shukurxan <wukurulloabduqayumov777@gmail.com>
2023-03-22 13:38:25 +1000

    progress

--- src/containers/skills.jsx
+++ src/containers/skills.jsx
@@ -127,6 +127,55 @@ export default class skills extends Component {
                     </div>
                 </div>
             </div>
+            <div className='mt-5'>
+                <h4 className='text-center text-white'>An Others</h4>
+            </div>
+            <div className='row mt-3'>
+                <div className='col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6'>
+                    <div class="container  mt-3">
+                        <div className='d-flex justify-content-between'>
+                            <span className='text-white'>WORD</span>
+                            <span className='text-white'>100%</span>
+                        </div>
+                        <div class="progress progress-striped10">
+                            <div class="progress-bar10">
+                            </div>                       
+                        </div> 
+                    </div>
+                    <div class="container  mt-3">
+                        <div className='d-flex justify-content-between'>
+                            <span className='text-white'>EXCEL</span>
+                            <span className='text-white'>70%</span>
+                        </div>
+                        <div class="progress progress-striped11">
+                            <div class="progress-bar11">
+                            </div>                       
+                        </div> 
+                    </div>
+                </div>
+                <div className='col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6'>
+                    <div class="container  mt-3">
+                        <div className='d-flex justify-content-between'>
+                            <span className='text-white'>POWER POINT</span>
+                            <span className='text-white'>90%</span>
+                        </div>
+                        <div class="progress progress-striped12">
+                            <div class="progress-bar12">
+                            </div>                       
+                        </div> 
+                    </div>
+                    <div class="container  mt-3">
+                        <div className='d-flex justify-content-between'>
+                            <span className='text-white'>PHOTOSHOP</span>
+                            <span className='text-white'>50%</span>
+                        </div>
+                        <div class="progress progress-striped13">
+                            <div class="progress-bar13">
+                            </div>                       
+                        </div> 
+                    </div>
+                </div>
+            </div>
         </div>
     )
   }
