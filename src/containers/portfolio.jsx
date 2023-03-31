d948e243e72cbb3cd4077af8c31f47880030f5a2
Shukurxan <wukurulloabduqayumov777@gmail.com>
2023-04-01 01:32:59 +1000

    portfolio

--- src/containers/portfolio.jsx
+++ src/containers/portfolio.jsx
@@ -1,126 +1,89 @@
 import React, { Component } from 'react';
 import '../css/portfolio.css';
-// import '../js/portfolio.js';
+import '../js/portfolio.js';
 
 export default class portfolio extends Component {
   render() {
     return (
-      <div className='p-3'>
-        <div id='port-div'>
-          <h3>Portfolio</h3>
-          <hr />
+        <div className='p-3'>
+            <div id='port-div'>
+            <h3>Portfolio</h3>
+            <hr />
+            </div>
+            <div class="container">
+                <div class="row">
+
+                <div align="center">
+                    <button class="btn btn-default filter-button" data-filter="all">All</button>
+                    <button class="btn btn-default filter-button" data-filter="hdpe">App</button>
+                    <button class="btn btn-default filter-button" data-filter="sprinkle">Card</button>
+                    <button class="btn btn-default filter-button" data-filter="spray">Web</button>
+                </div>
+                <br/>
+                    <div class="gallery_product col-12 col-sm-12 col-md-6 col-lg-4 col-xs-4 filter hdpe">
+                        <img src="" class="img-fluid" />
+                    </div>
+
+                    <div class="gallery_product col-12 col-sm-12 col-md-6 col-lg-4 col-xs-4 filter sprinkle">
+                        <img src="" class="img-fluid" />
+                    </div>
+
+                    <div class="gallery_product col-12 col-sm-12 col-md-6 col-lg-4 col-xs-4 filter hdpe">
+                        <img src="" class="img-fluid" />
+                    </div>
+
+                    <div id='img-div' class="gallery_product col-12 col-sm-12 col-md-6 col-lg-4 col-xs-4 filter spray">
+                        <div className='position-relative'>
+                            <img src="assets/image_2023-03-29_15-38-28.png" class="img-fluid" />
+                            <div id="overlay" className='row d-flex justify-content-between'>
+                                <div className='col-6'>
+                                    <span id='port-link' className='d-block text-center' data-bs-target="#exampleModalToggle" data-bs-toggle="modal"><i class="fa-solid fa-plus"></i></span>
+                                </div>
+                                <div className='col-6 border-start border-white'>
+                                    <a id='port-link' className='d-block text-center' href="https://fastidious-taffy-917333.netlify.app"><i class="fa-solid fa-link"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal fade w-100 modal-lg" id="exampleModalToggle" aria-hidden="true" aria-labelledby="exampleModalToggleLabel" tabindex="-1">
+                        <div id='web-img' class="modal-dialog modal-dialog-centered"></div>
+                    </div>
+
+                    <div class="gallery_product col-12 col-sm-12 col-md-6 col-lg-4 col-xs-4 filter sprinkle">
+                        <img src="" class="img-fluid" />
+                    </div>
+
+                    <div id='img-div' class="gallery_product col-12 col-sm-12 col-md-6 col-lg-4 col-xs-4 filter spray">
+                        <div className='position-relative'>
+                            <img src="assets/image_2023-03-31_21-58-25.png" class="img-fluid" />
+                            <div id="overlay" className='row d-flex justify-content-between'>
+                                <div className='col-6'>
+                                    <span id='port-link' className='d-block text-center' data-bs-target="#exampleModalToggle1" data-bs-toggle="modal"><i class="fa-solid fa-plus"></i></span>
+                                </div>
+                                <div className='col-6 border-start border-white'>
+                                    <a id='port-link' className='d-block text-center' href="https://helpful-gecko-348301.netlify.app"><i class="fa-solid fa-link"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal fade w-100 modal-lg" id="exampleModalToggle1" aria-hidden="true" aria-labelledby="exampleModalToggleLabel" tabindex="-1">
+                        <div id='web-img1' class="modal-dialog modal-dialog-centered"></div>
+                    </div>
+
+                    <div class="gallery_product col-12 col-sm-12 col-md-6 col-lg-4 col-xs-4 filter hdpe">
+                        <img src="" class="img-fluid" />
+                    </div>
+
+                    <div class="gallery_product col-12 col-sm-12 col-md-6 col-lg-4 col-xs-4 filter spray">
+                        <img src="" class="img-fluid" />
+                    </div>
+
+                    <div class="gallery_product col-12 col-sm-12 col-md-6 col-lg-4 col-xs-4 filter sprinkle">
+                        <img src="" class="img-fluid" />
+                    </div>
+                </div>
+            </div>
         </div>
-        <section id="portfolio" class="portfolio section-bg mt-4">
-          <div class="container">
-
-            <div class="row" data-aos="fade-up">
-              <div class="col-lg-12 d-flex justify-content-center">
-                <ul id="portfolio-flters">
-                  <li data-filter="*" class="filter-active">All</li>
-                  <li data-filter=".filter-app">App</li>
-                  <li data-filter=".filter-card">Card</li>
-                  <li data-filter=".filter-web">Web</li>
-                </ul>
-              </div>
-            </div>
-
-            <div class="row portfolio-container" data-aos="fade-up" data-aos-delay="100">
-
-              <div class="col-lg-4 col-md-6 portfolio-item filter-app">
-                <div class="portfolio-wrap">
-                  <img src="assets/image_2023-03-29_15-38-28.png" class="img-fluid" alt="" />
-                  <div class="portfolio-links">
-                    <a href="assets/image_2023-03-29_15-38-28.png" data-gallery="portfolioGallery" class="portfolio-lightbox" title="App 1"><i class="bx bx-plus"></i></a>
-                    <a href="https://fastidious-taffy-917333.netlify.app/" title="More Details"><i class="bx bx-link"></i></a>
-                  </div>
-                </div>
-              </div>
-
-              <div class="col-lg-4 col-md-6 portfolio-item filter-web">
-                <div class="portfolio-wrap">
-                  <img src="assets/img/portfolio/portfolio-2.jpg" class="img-fluid" alt="" />
-                  <div class="portfolio-links">
-                    <a href="assets/img/portfolio/portfolio-2.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Web 3"><i class="bx bx-plus"></i></a>
-                    <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>
-                  </div>
-                </div>
-              </div>
-
-              <div class="col-lg-4 col-md-6 portfolio-item filter-app">
-                <div class="portfolio-wrap">
-                  <img src="assets/img/portfolio/portfolio-3.jpg" class="img-fluid" alt="" />
-                  <div class="portfolio-links">
-                    <a href="assets/img/portfolio/portfolio-3.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="App 2"><i class="bx bx-plus"></i></a>
-                    <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>
-                  </div>
-                </div>
-              </div>
-
-              <div class="col-lg-4 col-md-6 portfolio-item filter-card">
-                <div class="portfolio-wrap">
-                  <img src="assets/img/portfolio/portfolio-4.jpg" class="img-fluid" alt="" />
-                  <div class="portfolio-links">
-                    <a href="assets/img/portfolio/portfolio-4.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Card 2"><i class="bx bx-plus"></i></a>
-                    <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>
-                  </div>
-                </div>
-              </div>
-
-              <div class="col-lg-4 col-md-6 portfolio-item filter-web">
-                <div class="portfolio-wrap">
-                  <img src="assets/img/portfolio/portfolio-5.jpg" class="img-fluid" alt="" />
-                  <div class="portfolio-links">
-                    <a href="assets/img/portfolio/portfolio-5.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Web 2"><i class="bx bx-plus"></i></a>
-                    <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>
-                  </div>
-                </div>
-              </div>
-
-              <div class="col-lg-4 col-md-6 portfolio-item filter-app">
-                <div class="portfolio-wrap">
-                  <img src="assets/img/portfolio/portfolio-6.jpg" class="img-fluid" alt="" />
-                  <div class="portfolio-links">
-                    <a href="assets/img/portfolio/portfolio-6.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="App 3"><i class="bx bx-plus"></i></a>
-                    <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>
-                  </div>
-                </div>
-              </div>
-
-              <div class="col-lg-4 col-md-6 portfolio-item filter-card">
-                <div class="portfolio-wrap">
-                  <img src="assets/img/portfolio/portfolio-7.jpg" class="img-fluid" alt="" />
-                  <div class="portfolio-links">
-                    <a href="assets/img/portfolio/portfolio-7.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Card 1"><i class="bx bx-plus"></i></a>
-                    <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>
-                  </div>
-                </div>
-              </div>
-
-              <div class="col-lg-4 col-md-6 portfolio-item filter-card">
-                <div class="portfolio-wrap">
-                  <img src="assets/img/portfolio/portfolio-8.jpg" class="img-fluid" alt="" />
-                  <div class="portfolio-links">
-                    <a href="assets/img/portfolio/portfolio-8.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Card 3"><i class="bx bx-plus"></i></a>
-                    <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>
-                  </div>
-                </div>
-              </div>
-
-              <div class="col-lg-4 col-md-6 portfolio-item filter-web">
-                <div class="portfolio-wrap">
-                  <img src="assets/img/portfolio/portfolio-9.jpg" class="img-fluid" alt="" />
-                  <div class="portfolio-links">
-                    <a href="assets/img/portfolio/portfolio-9.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Web 3"><i class="bx bx-plus"></i></a>
-                    <a href="portfolio-details.html" title="More Details"><i class="bx bx-link"></i></a>
-                  </div>
-                </div>
-              </div>
-
-            </div>
-
-          </div>
-        </section>
-      </div>
     )
   }
 }
--- src/js/portfolio.js
+++ src/js/portfolio.js
@@ -1,44 +1,27 @@
-window.addEventListener('load', () => {
-    let portfolioContainer = document.getElementsByClassName('portfolio-container');
-    if (portfolioContainer) {
-      let portfolioIsotope = new Isotope(portfolioContainer, {
-        itemSelector: '.portfolio-item'
-      });
+import $ from 'jquery';
+$(document).ready(function(){
 
-      let portfolioFilters = select('#portfolio-flters li', true);
-
-      on('click', '#portfolio-flters li', function(e) {
-        e.preventDefault();
-        portfolioFilters.forEach(function(el) {
-          el.classList.remove('filter-active');
-        });
-        this.classList.add('filter-active');
-
-        portfolioIsotope.arrange({
-          filter: this.getAttribute('data-filter')
-        });
-        portfolioIsotope.on('arrangeComplete', function() {
-          AOS.refresh()
-        });
-      }, true);
-    }
-
+  $(".filter-button").click(function(){
+      var value = $(this).attr('data-filter');
+      
+      if(value == "all")
+      {
+          //$('.filter').removeClass('hidden');
+          $('.filter').show('1000');
+      }
+      else
+      {
+//            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+//            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+          $(".filter").not('.'+value).hide('3000');
+          $('.filter').filter('.'+value).show('3000');
+          
+      }
   });
+  
+  if ($(".filter-button").removeClass("active")) {
+$(this).removeClass("active");
+}
+$(this).addClass("active");
 
-  const portfolioLightbox = GLightbox({
-    selector: '.portfolio-lightbox'
-  });
-
-  new Swiper('.portfolio-details-slider', {
-    speed: 400,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    }
-  });
+});
